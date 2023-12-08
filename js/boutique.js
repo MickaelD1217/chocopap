25fab249c38050e89ee960dd914d1bc4a3cdb8d4
MickaelD1217 <mickael.daures@free.fr>
2023-12-08 17:20:14 +0100

    correctif total commande

--- js/boutique.js
+++ js/boutique.js
@@ -183,21 +183,21 @@ let lstProducts = `[{
          "ingredients": "Caramel au beurre salé avec un coeur en poudre chocolatée. Allergènes : SOJA, LAIT (LACTOSE). Contient de l'anhydride sulfureux"
      }
  ]`;
- let myProducts = JSON.parse(lstProducts);
+let myProducts = JSON.parse(lstProducts);
 
 // console.log(myProducts);
 let myCaseTous = document.getElementById('checkBoxTous');
 let caseAcocher = document.querySelectorAll('#checkbox input');
 let viewCaddy = document.getElementById('myCaddy');
 viewCaddy.addEventListener('shown.bs.modal', affPanier, false);
+viewCaddy.addEventListener('hidden.bs.modal', compteArticle, false);
 
-
-let btnVidePanier =document.getElementById('btnVidePanier');
+let btnVidePanier = document.getElementById('btnVidePanier');
 btnVidePanier.addEventListener('click', videPanier, false);
 
 //initialise un évenement, execute la procedure clickSurCaseACocher, 
 //chaque fois que l'on sélectionne une case à cocher
-for(let i=0; i < caseAcocher.length; i++){
+for (let i = 0; i < caseAcocher.length; i++) {
   let myCase = caseAcocher[i];
   myCase.addEventListener('click', clickSurCaseAcocher, false);
 }
@@ -205,78 +205,78 @@ for(let i=0; i < caseAcocher.length; i++){
 //initialise un évenement, execute la procedure changeSurSelecteur
 //chaque fois qu'on change la valeur d'un sélecteur
 let selecteur = document.querySelectorAll('#checkbox select');
-for(let i=0; i < selecteur.length; i++){
+for (let i = 0; i < selecteur.length; i++) {
   let myCase = selecteur[i];
   myCase.addEventListener('change', changeSurSelecteur, false);
 }
 
 //Est executer lorsqu'on clique sur le panier pour afficher la fenetre MODALE
-function affPanier(myEvent){
+function affPanier(myEvent) {
   let mesCommandes = document.getElementById('maCommande');
-  while(mesCommandes.firstChild) {
+  while (mesCommandes.firstChild) {
     mesCommandes.removeChild(mesCommandes.firstChild);
   }
   let totCommande = 0;
-  for (var i=0;i<caddies.length;i++){
-    
-    if ( caddies[i]>0 ){
+  for (var i = 0; i < caddies.length; i++) {
+
+    if (caddies[i] > 0) {
       let unProduits = myProducts[i];
       totCommande += Number(unProduits.price) * caddies[i];
 
-        let elemArticle = document.createElement('article');
-        elemArticle.className = "row"
-        let elemDivImg = document.createElement('div');
-        elemDivImg.className = "col-3 d-flex justify-content-around align-self-center";
-        let elemImgSup = document.createElement('i');
-        elemImgSup.className = "fa-solid fa-xmark"
-        elemImgSup.style.color = "#ff0000"
-        let elemImgProduits = document.createElement('img');
-        elemImgProduits.style.maxWidth = "50px";
-        elemImgProduits.src = unProduits.image;
-        elemDivImg.appendChild(elemImgSup);
-        elemDivImg.appendChild(elemImgProduits);
-        elemArticle.appendChild(elemDivImg);
-        let elemDivRef = document.createElement('div');
-        elemDivRef.className = "col-4";
-        let elmH5Title = document.createElement('h6');
-        elmH5Title.textContent = unProduits.title;
-        let elmH5Prix = document.createElement('h6');
-        elmH5Prix.textContent  = unProduits.price+" €";
-        elemDivRef.appendChild(elmH5Title);
-        elemDivRef.appendChild(elmH5Prix);
-        elemArticle.appendChild(elemDivRef);
-        let elemDivQte = document.createElement('div');
-        elemDivQte.className = "col-2";
-        let Qte = document.createElement('input')
-        Qte.type = "number";
-        Qte.className = "form-control form-control-sm";
-        Qte.placeholder = caddies[i];
-        Qte.value = caddies[i];
-        elemDivQte.appendChild(Qte);
-        elemArticle.appendChild(elemDivQte);
-        mesCommandes.appendChild(elemArticle);
-      }
-   }
-   document.getElementById("totalCommande").textContent = "Total : "+totCommande+" €";
+      let elemArticle = document.createElement('article');
+      elemArticle.className = "row"
+      let elemDivImg = document.createElement('div');
+      elemDivImg.className = "col-3 d-flex justify-content-around align-self-center";
+      let elemImgSup = document.createElement('i');
+      elemImgSup.className = "fa-solid fa-xmark"
+      elemImgSup.style.color = "#ff0000"
+      let elemImgProduits = document.createElement('img');
+      elemImgProduits.style.maxWidth = "50px";
+      elemImgProduits.src = unProduits.image;
+      elemDivImg.appendChild(elemImgSup);
+      elemDivImg.appendChild(elemImgProduits);
+      elemArticle.appendChild(elemDivImg);
+      let elemDivRef = document.createElement('div');
+      elemDivRef.className = "col-4";
+      let elmH5Title = document.createElement('h6');
+      elmH5Title.textContent = unProduits.title;
+      let elmH5Prix = document.createElement('h6');
+      elmH5Prix.textContent = unProduits.price + " €";
+      elemDivRef.appendChild(elmH5Title);
+      elemDivRef.appendChild(elmH5Prix);
+      elemArticle.appendChild(elemDivRef);
+      let elemDivQte = document.createElement('div');
+      elemDivQte.className = "col-2";
+      let Qte = document.createElement('input')
+      Qte.type = "number";
+      Qte.className = "form-control form-control-sm";
+      Qte.placeholder = caddies[i];
+      Qte.value = caddies[i];
+      elemDivQte.appendChild(Qte);
+      elemArticle.appendChild(elemDivQte);
+      mesCommandes.appendChild(elemArticle);
+    }
+  }
+  document.getElementById("totalCommande").textContent = "Total : " + totCommande.toFixed(2) + " €";
 }
 
-function changeSurSelecteur(myEvent){
+function changeSurSelecteur(myEvent) {
   let cible = myEvent.target.id;
   let newValue = Number(myEvent.target.value);
   //console.log(cible+" "+myEvent.target.value);
-  let max= document.getElementById("prixMax");
-  let min= document.getElementById("prixMin");
+  let max = document.getElementById("prixMax");
+  let min = document.getElementById("prixMin");
 
-  switch(cible){
+  switch (cible) {
     case "prixMin":
-      if (newValue > Number(max.value)){
-        max.value=myEvent.target.value;
+      if (newValue > Number(max.value)) {
+        max.value = myEvent.target.value;
         console.error("prix min > prix max");
       }
       break;
     case "prixMax":
-      if (newValue < Number(min.value)){
-        min.value=myEvent.target.value;
+      if (newValue < Number(min.value)) {
+        min.value = myEvent.target.value;
         console.error("prix max < prix min");
       }
       break;
@@ -284,25 +284,25 @@ function changeSurSelecteur(myEvent){
       break;
     case "noteMax":
       break;
-            
+
   }
   delArticles();
   affArticles();
-  
+
 
   //console.log(myEvent.target.id+" "+myEvent.target.value);
 }
 
-function clickSurCaseAcocher(myEvent){
+function clickSurCaseAcocher(myEvent) {
   let myCase = document.getElementById(myEvent.target.id);
-  
-  if (myEvent.target.id=="checkBoxTous" && myCase.checked){
-    for(let i=1; i < caseAcocher.length; i++){
+
+  if (myEvent.target.id == "checkBoxTous" && myCase.checked) {
+    for (let i = 1; i < caseAcocher.length; i++) {
       let myCase = caseAcocher[i];
       myCase.checked = false;
     }
-  }else if (myCase.checked){
-    myCaseTous.checked=false;
+  } else if (myCase.checked) {
+    myCaseTous.checked = false;
   }
   delArticles();
   affArticles();
@@ -312,126 +312,126 @@ function clickSurCaseAcocher(myEvent){
   //  }else{
   //    console.log(myEvent.target.id+" false");
   //  }
-  
+
 }
 
 //Constitution d'un article dans le DOM
-function newArticles(id,fileImage,libelle,prix,etoile=2){
+function newArticles(id, fileImage, libelle, prix, etoile = 2) {
   let elemImg = document.createElement('img');
   // elemImg.src = `images/produit${id}.jpg`;
   elemImg.src = fileImage;
   elemImg.alt = libelle;
   elemImg.className = 'w-100 p-2';
   let elemH5 = document.createElement('h5');
-  elemH5.textContent=libelle;
+  elemH5.textContent = libelle;
   let elemPrix = document.createElement('p');
-  elemPrix.textContent=prix+' €';
-  
+  elemPrix.textContent = prix + ' €';
+
   let elemBtn = document.createElement('button');
-  elemBtn.textContent='ajouter au panier';
-  elemBtn.className='btn btn-primary';
-  elemBtn.type='button';
-  elemBtn.id=id;
-  
+  elemBtn.textContent = 'ajouter au panier';
+  elemBtn.className = 'btn btn-primary';
+  elemBtn.type = 'button';
+  elemBtn.id = id;
+
   let elemProduit = document.createElement('article');
   elemProduit.className = 'col-12 col-sm-6 col-md-4 pb-1';
   elemProduit.appendChild(elemImg);
   elemProduit.appendChild(elemH5);
   elemProduit.appendChild(elemPrix);
-  (function(note){
+  (function (note) {
     let elemNote = document.createElement('div');
-    elemNote.style.height='35px';
-    
+    elemNote.style.height = '35px';
+
     let elemImgEtoilePleine = document.createElement('i');
-    elemImgEtoilePleine.className='fa-solid fa-star';
-    elemImgEtoilePleine.style.color='#ffc800';
-    for(var i=1; i<= note; i+=1){
-      if(i==1) { 
-        elemNote.appendChild(elemImgEtoilePleine); 
+    elemImgEtoilePleine.className = 'fa-solid fa-star';
+    elemImgEtoilePleine.style.color = '#ffc800';
+    for (var i = 1; i <= note; i += 1) {
+      if (i == 1) {
+        elemNote.appendChild(elemImgEtoilePleine);
       }
       else {
-        elemNote.appendChild(elemImgEtoilePleine.cloneNode()); 
+        elemNote.appendChild(elemImgEtoilePleine.cloneNode());
       }
-   }  
-   let elemImgEtoileVide = document.createElement('i');
-   elemImgEtoileVide.className='fa-regular fa-star';
-   elemImgEtoileVide.style.color='#ffc800';
-   for(var k=(note+1); k<6; k+=1){
-    if(k==(note+1)) { 
-      elemNote.appendChild(elemImgEtoileVide); 
     }
-    else {
-      elemNote.appendChild(elemImgEtoileVide.cloneNode()); 
+    let elemImgEtoileVide = document.createElement('i');
+    elemImgEtoileVide.className = 'fa-regular fa-star';
+    elemImgEtoileVide.style.color = '#ffc800';
+    for (var k = (note + 1); k < 6; k += 1) {
+      if (k == (note + 1)) {
+        elemNote.appendChild(elemImgEtoileVide);
+      }
+      else {
+        elemNote.appendChild(elemImgEtoileVide.cloneNode());
+      }
     }
-   }
-   elemProduit.appendChild(elemNote);
+    elemProduit.appendChild(elemNote);
   })(etoile);
-  
+
   elemProduit.appendChild(elemBtn);
-  
+
   // elemProduit.appendChild(elemNote);
   lstArticles.appendChild(elemProduit);
-  
+
 }
 
 //Supprime tous les articles du DOM
-function delArticles(){
-  while(lstArticles.firstChild) {
+function delArticles() {
+  while (lstArticles.firstChild) {
     lstArticles.removeChild(lstArticles.firstChild);
   }
 }
 
 //Affiche les articles en fonction des choix cases à cocher et filtre
-function affArticles(){
-  var compteur=0;
-  for (unProduits of myProducts){
+function affArticles() {
+  var compteur = 0;
+  for (unProduits of myProducts) {
     let bAffiche = false;
     let bPrix = false;
     let bNote = false;
-    if (myCaseTous.checked){
+    if (myCaseTous.checked) {
       bAffiche = true;
-    }else{
-      for(let i=1; i < caseAcocher.length; i++){
+    } else {
+      for (let i = 1; i < caseAcocher.length; i++) {
         let myCase = caseAcocher[i];
-        if (myCase.checked){
-          if (myCase.id=='checkBoxChocoBlanc' && unProduits.category.blanc){bAffiche = true;}
-          else if (myCase.id=='checkBoxChocoLait' && unProduits.category.lait){bAffiche = true;}
-          else if (myCase.id=='checkBoxChocoNoir' && unProduits.category.noir){bAffiche = true;}
-          else if (myCase.id=='checkBoxNoisette' && unProduits.category.noix){bAffiche = true;}
-          else if (myCase.id=='checkBoxFruit' && unProduits.category.fruit){bAffiche = true;}
-          else if (myCase.id=='checkBoxCaramel' && unProduits.category.caramel){bAffiche = true;}
-          else if (myCase.id=='checkBoxLiqueur' && unProduits.category.liqueur){bAffiche = true;}
+        if (myCase.checked) {
+          if (myCase.id == 'checkBoxChocoBlanc' && unProduits.category.blanc) { bAffiche = true; }
+          else if (myCase.id == 'checkBoxChocoLait' && unProduits.category.lait) { bAffiche = true; }
+          else if (myCase.id == 'checkBoxChocoNoir' && unProduits.category.noir) { bAffiche = true; }
+          else if (myCase.id == 'checkBoxNoisette' && unProduits.category.noix) { bAffiche = true; }
+          else if (myCase.id == 'checkBoxFruit' && unProduits.category.fruit) { bAffiche = true; }
+          else if (myCase.id == 'checkBoxCaramel' && unProduits.category.caramel) { bAffiche = true; }
+          else if (myCase.id == 'checkBoxLiqueur' && unProduits.category.liqueur) { bAffiche = true; }
         }
       }
     }
-    let price=Number(unProduits.price)
-    let max= Number(document.getElementById("prixMax").value);
-    let min= Number(document.getElementById("prixMin").value);
-    if (price>=min && price<=max) {bPrix=true;}
+    let price = Number(unProduits.price)
+    let max = Number(document.getElementById("prixMax").value);
+    let min = Number(document.getElementById("prixMin").value);
+    if (price >= min && price <= max) { bPrix = true; }
 
-    let quote=unProduits.note
-    let quotemax= document.getElementById("noteMax").value;
-    let quotemin= document.getElementById("noteMin").value;
-    if (quote>=quotemin && quote<=quotemax) {bNote=true;}
+    let quote = unProduits.note
+    let quotemax = document.getElementById("noteMax").value;
+    let quotemin = document.getElementById("noteMin").value;
+    if (quote >= quotemin && quote <= quotemax) { bNote = true; }
 
-    if (bAffiche && bPrix && bNote){
-      newArticles(unProduits.id,unProduits.image,unProduits.title,unProduits.price,unProduits.note);  
+    if (bAffiche && bPrix && bNote) {
+      newArticles(unProduits.id, unProduits.image, unProduits.title, unProduits.price, unProduits.note);
       compteur++;
     }
-    
-       //console.log(myProducts[unProduits].title);
+
+    //console.log(myProducts[unProduits].title);
   }
-  
-  if (compteur==0){
+
+  if (compteur == 0) {
     let elemH2 = document.createElement('h2');
-    elemH2.textContent="Aucun produit ne correspond à la recherche";
+    elemH2.textContent = "Aucun produit ne correspond à la recherche";
     lstArticles.appendChild(elemH2);
-  }else{
+  } else {
     //initialise un évenement, execute la procedure clickSurCaseACocher, 
     //chaque fois que l'on sélectionne une case à cocher
     btnAddCaddy = document.querySelectorAll('#myArticle button');
 
-    for(let i=0; i < btnAddCaddy.length; i++){
+    for (let i = 0; i < btnAddCaddy.length; i++) {
       let myBtn = btnAddCaddy[i];
       myBtn.addEventListener('click', clickSurAjouterPanier, false);
     }
@@ -443,77 +443,81 @@ function affArticles(){
 //CODE PRINCIPAL
 affArticles();
 
-var caddies = new Array(0,0,0,0,0,0,0,0,0,0);
-let menuPanier = document.getElementById('shopping');
+var caddies = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
 
 if (!localStorage.getItem("caddies")) {
 } else {
-   let texte = new Array();
-   texte = localStorage.getItem("caddies").split(",");
-    for (var i=0;i<texte.length;i++){
-      caddies[i] = Number(texte[i]);
-   }
+  let texte = new Array();
+  texte = localStorage.getItem("caddies").split(",");
+  for (var i = 0; i < texte.length; i++) {
+    caddies[i] = Number(texte[i]);
+  }
 }
 
 
-function clickSurAjouterPanier(myEvent){
-   // let myBouton = document.getElementById(myEvent.target.id);
-   let myBouton = document.getElementById(myEvent.target.id);
+function clickSurAjouterPanier(myEvent) {
+  // let myBouton = document.getElementById(myEvent.target.id);
+  let myBouton = document.getElementById(myEvent.target.id);
   //  console.log(myBouton.id);
-   caddies[Number(myBouton.id)-1]++;
-   compteArticle();
- }
- 
-function videPanier(){
-   for (var i=0;i<caddies.length;i++){
-      caddies[i]=0;
-   }
-   compteArticle(); //sauvegarde aussi le panier
+  caddies[Number(myBouton.id) - 1]++;
+  compteArticle();
 }
 
-function sauvePanier(){
-   //Attention sauvegarde le tableau comme une chaine 
-   localStorage.setItem('caddies',caddies); 
+function videPanier() {
+  for (var i = 0; i < caddies.length; i++) {
+    caddies[i] = 0;
+  }
+  compteArticle(); //sauvegarde aussi le panier
+}
+
+function sauvePanier() {
+  //Attention sauvegarde le tableau comme une chaine 
+  localStorage.setItem('caddies', caddies);
 }
 
 //Modifie le menu avec le nombre d'article en cours
-function compteArticle(){
-   let nbArticle = 0;
-   for (var i=0;i<caddies.length;i++){
-      nbArticle += caddies[i];
-   }
-   if (nbArticle==0){
-      if (!menuPanier.firstChild.textContent == "Panier"){
-         while(menuPanier.firstChild) {
-            menuPanier.removeChild(menuPanier.firstChild);
-         }
-         menuPanier.textContent("Panier");
+function compteArticle() {
+  let menuPanier = document.getElementById('shopping');
+  let nbArticle = 0;
+  for (var i = 0; i < caddies.length; i++) {
+    nbArticle += caddies[i];
+  }
+
+  if (nbArticle == 0) {
+
+    if (menuPanier.firstChild.textContent != "Panier") {
+      while (menuPanier.firstChild) {
+        menuPanier.removeChild(menuPanier.firstChild);
       }
-      if (localStorage.getItem("caddies")){
-         localStorage.removeItem("caddies");
+      menuPanier.textContent = "Panier";
+    }
+    if (localStorage.getItem("caddies")) {
+      localStorage.removeItem("caddies");
+    }
+  } else {
+    if (menuPanier.firstChild.textContent == "Panier") {
+      while (menuPanier.firstChild) {
+        menuPanier.removeChild(menuPanier.firstChild);
       }
-   }else{
-      if (menuPanier.firstChild.textContent == "Panier"){
-         while(menuPanier.firstChild) {
-            menuPanier.removeChild(menuPanier.firstChild);
-         }
-         // <i class="position-relative fa-solid fa-cart-shopping fa-2xl">
-         // <h6 class="position-absolute top-0 start-0 translate-middle p-1 mb-1 bg-dark text-light rounded-circle" id="nbreArticles">15</h6>
-         // </i>
-         let elemImg = document.createElement('i');
-         elemImg.className = 'position-relative fa-solid fa-cart-shopping fa-2xl';
-         elemImg.dataset.bsToggle='modal';
-         elemImg.dataset.bsTarget='#myCaddy';
-         let elemH6 = document.createElement('h6');
-         elemH6.className = 'position-absolute top-0 start-0 translate-middle p-1 mb-1 bg-dark text-light rounded-circle';
-         elemH6.id = 'nbreArticles';
-         elemImg.appendChild(elemH6);
-         menuPanier.appendChild(elemImg);
-      }
-      let monPanier = document.getElementById('nbreArticles');
-      monPanier.textContent=nbArticle;
-      sauvePanier();
-   }
+      // <i class="position-relative fa-solid fa-cart-shopping fa-2xl">
+      // <h6 class="position-absolute top-0 start-0 translate-middle p-1 mb-1 bg-dark text-light rounded-circle" id="nbreArticles">15</h6>
+      // </i>
+      let elemImg = document.createElement('i');
+      elemImg.className = 'position-relative fa-solid fa-cart-shopping fa-2xl';
+      elemImg.dataset.bsToggle = 'modal';
+      elemImg.dataset.bsTarget = '#myCaddy';
+      let elemH6 = document.createElement('h6');
+      elemH6.className = 'position-absolute top-0 start-0 translate-middle p-1 mb-1 bg-dark text-light rounded-circle';
+      console.log(elemH6);
+      elemH6.id = 'nbreArticles';
+      elemImg.appendChild(elemH6);
+      menuPanier.appendChild(elemImg);
+    }
+    let monPanier = document.getElementById('nbreArticles');
+    monPanier.textContent = nbArticle;
+    sauvePanier();
+  }
 }
 
 compteArticle();
